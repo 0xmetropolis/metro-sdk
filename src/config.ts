9f35afbc347e5137122389c33d88e75fbe40cf9a
Will Kim <will@sonar-labs.com>
2022-03-25 13:14:32 -0400

    fix: Rinkeby init now requires a subgraphUrl

--- src/config.ts
+++ src/config.ts
@@ -18,9 +18,19 @@ export function init({
   network: number;
   subgraphUrl?: string;
 }) {
+  config.network = network;
+
   // Only accept 1 or 4 for network values.
   if (!(network === 1 || network === 4)) {
     throw new Error('Network can only be 1 or 4 (mainnet or rinkeby)');
+  }
+
+  if (network === 4) {
+    if (!subgraphUrl) throw new Error('Must provide subgraph URL for testnet');
+    config.subgraphUrl = subgraphUrl;
+  } else if (network === 1) {
+    config.subgraphUrl =
+      'https://gateway.thegraph.com/api/50ba04ffb3711883f1fd80df4f93242b/subgraphs/id/0x3f4e2cfe11aa607570e0aee7ac74fbff9633fa8e-0';
   }
 
   config.provider = provider;
@@ -31,18 +41,6 @@ export function init({
     if (err.message.includes('does not support signing')) {
       config.provider.getSigner = () => {};
     }
-  }
-
-  config.network = network;
-
-  // Override subgraph URL.
-  if (subgraphUrl) {
-    config.subgraphUrl = subgraphUrl;
-  } else {
-    config.subgraphUrl =
-      network === 1
-        ? 'https://gateway.thegraph.com/api/50ba04ffb3711883f1fd80df4f93242b/subgraphs/id/0x3f4e2cfe11aa607570e0aee7ac74fbff9633fa8e-0'
-        : 'https://api.studio.thegraph.com/query/3720/orca-1/v0.1.2';
   }
 
   config.gnosisUrl =
