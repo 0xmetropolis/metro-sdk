9f5632859f73b570f273da8eeba86c7d483320f7
Will Kim <will@sonar-labs.com>
2022-07-13 14:39:41 -0400

    feat: multi pod create

--- scripts/sandbox.ts
+++ scripts/sandbox.ts
@@ -1,16 +1,45 @@
 import { ethers } from 'ethers';
 import { init } from '../src';
-import { getPod } from '../src';
-import { adminPodAddress } from '../env.json';
+import { getPod, multiPodCreate } from '../src';
+import { accountOne, accountTwo, adminPodAddress } from '../env.json';
 import { setup, sleep } from './utils';
 
-async function main() {
-  const { walletOne, dummyAccount } = setup(1);
+const multiPodInput = [
+  {
+    label: 'multi-parent-c',
+    members: ['multi-child-c', accountOne],
+    threshold: 1,
+    admin: accountOne,
+  },
+  {
+    label: 'multi-child-c',
+    members: [accountOne, accountTwo],
+    threshold: 1,
+  },
+  {
+    label: 'multi-child2-c',
+    members: [accountOne, accountTwo],
+    threshold: 1,
+    admin: 'multi-parent-c',
+  },
+];
 
-  const pod = await getPod('eject.pod.xyz');
-  console.log('pod', pod);
-  // console.log('dummyAccount', dummyAccount);
-  // await pod.ejectSafe(dummyAccount);
+async function main() {
+  const { walletOne } = setup(4);
+
+  // await multiPodCreate(multiPodInput, walletOne);
+  // const multiParent = await getPod('multi-parent-c.pod.eth');
+  // console.log('multiParent', multiParent);
+  // const members = await multiParent.getMembers();
+  // console.log(await multiParent.getMembers());
+
+  // const multiChild = await getPod('multi-child-c.pod.eth');
+  // console.log('multiParent', multiChild);
+  // console.log(await multiChild.getMembers());
+
+  // const multiChild2 = await getPod('multi-child2-c.pod.eth');
+  // console.log('multiParent', multiChild2);
+  // console.log(await multiChild2.getMembers());
 }
 
 main();
--- test/pod-create.test.ts
+++ test/pod-create.test.ts
@@ -1,7 +1,6 @@
 import { ethers } from 'ethers';
 import { multiPodCreate } from '../src/pod-create';
 import { init } from '../src';
-import * as create from '../src/temp';
 import { userAddress, userAddress2 } from './fixtures';
 
 beforeAll(async () => {
@@ -9,6 +8,10 @@ beforeAll(async () => {
     infura: '69ecf3b10bc24c6a972972666fe950c8',
   });
   init({ provider, network: 4 });
+});
+
+beforeEach(() => {
+  jest.restoreAllMocks();
 });
 
 const multiPodInput = [
@@ -31,36 +34,45 @@ const multiPodInput = [
 ];
 
 describe('multi pod create', () => {
-  test('It should replace labels with pod indices', async () => {
-    const mockCreate = jest.spyOn(create, 'createPods');
-    jest.spyOn(ethers, 'Contract').mockReturnValueOnce({
-      getNextAvailablePodId: jest.fn().mockResolvedValueOnce({
-        toNumber: jest.fn().mockReturnValueOnce(100),
-      }),
-    });
+  test('It should call the smart contract with the appropriate formatting', async () => {
+    const mockCreate = jest.fn();
+    jest
+      .spyOn(ethers, 'Contract')
+      .mockReturnValueOnce({
+        getNextAvailablePodId: jest.fn().mockResolvedValueOnce({
+          toNumber: jest.fn().mockReturnValueOnce(100),
+        }),
+      })
+      .mockReturnValueOnce({
+        createPods: mockCreate,
+      });
     await multiPodCreate(multiPodInput);
     expect(mockCreate).toHaveBeenCalledWith(
       '0x99a184725B2C12E5C8FBc1E3f680a5aDB3d07160',
       [
+        [userAddress, userAddress2],
+        [userAddress, userAddress2],
         [
-          '0x0000000000000000000000000000000000000001',
           '0x0000000000000000000000000000000000000002',
+          '0x0000000000000000000000000000000000000001',
         ],
-        [userAddress, userAddress2],
-        [userAddress, userAddress2],
       ],
       [1, 1, 1],
-      [ethers.constants.AddressZero, ethers.constants.AddressZero, ethers.constants.AddressZero],
       [
-        '0xcba04bae8ddd2442d7f24f0526838e2bc10c828cc09bea0cf6bbf31fe113b667',
-        '0x45fd18df471e91cc8d6eb6a2b1bf22bd2033c21c7987cf1a69017e31ed7722c0',
-        '0xf3d1321074b678d5bdd2c74fd5498bb4018cf1af83d64101a3c5a71b64e82ceb',
+        '0x0000000000000000000000000000000000000003',
+        ethers.constants.AddressZero,
+        ethers.constants.AddressZero,
       ],
-      ['orcanauts.pod.xyz', 'art-nauts.pod.xyz', 'gov-nauts.pod.xyz'],
       [
-        'https://orcaprotocol-nft.vercel.app/assets/testnet/0000000000000000000000000000000000000000000000000000000000000064-image',
-        'https://orcaprotocol-nft.vercel.app/assets/testnet/0000000000000000000000000000000000000000000000000000000000000065-image',
+        '0xf3d1321074b678d5bdd2c74fd5498bb4018cf1af83d64101a3c5a71b64e82ceb',
+        '0x45fd18df471e91cc8d6eb6a2b1bf22bd2033c21c7987cf1a69017e31ed7722c0',
+        '0xcba04bae8ddd2442d7f24f0526838e2bc10c828cc09bea0cf6bbf31fe113b667',
+      ],
+      ['gov-nauts.pod.xyz', 'art-nauts.pod.xyz', 'orcanauts.pod.xyz'],
+      [
         'https://orcaprotocol-nft.vercel.app/assets/testnet/0000000000000000000000000000000000000000000000000000000000000066-image',
+        'https://orcaprotocol-nft.vercel.app/assets/testnet/0000000000000000000000000000000000000000000000000000000000000065-image',
+        'https://orcaprotocol-nft.vercel.app/assets/testnet/0000000000000000000000000000000000000000000000000000000000000064-image',
       ],
     );
   });
@@ -75,7 +87,7 @@ describe('multi pod create', () => {
     const modifiedInput = multiPodInput;
     // Referencing a pod not in the array should throw.
     modifiedInput[1] = { ...modifiedInput[1], members: ['anotherpod'] };
-    await expect(multiPodCreate(multiPodInput)).rejects.toThrow('No pod had the label of');
+    await expect(multiPodCreate(modifiedInput)).rejects.toThrow('No pod had the label of');
   });
 
   test('If provided an admin label, it must reference another pod in the list', async () => {
@@ -88,36 +100,6 @@ describe('multi pod create', () => {
     const modifiedInput = multiPodInput;
     // Referencing a pod not in the array should throw.
     modifiedInput[1] = { ...modifiedInput[1], admin: 'anotherpod' };
-    await expect(multiPodCreate(multiPodInput)).rejects.toThrow('No pod had the label of');
-  });
-
-  test('Member pods cannot reference earlier pods in the create', async () => {
-    jest.spyOn(ethers, 'Contract').mockReturnValueOnce({
-      getNextAvailablePodId: jest.fn().mockResolvedValueOnce({
-        toNumber: jest.fn().mockReturnValueOnce(100),
-      }),
-    });
-
-    const modifiedInput = multiPodInput;
-    // Referencing an earlier pod should throw.
-    modifiedInput[1] = { ...modifiedInput[1], members: ['orcanauts'] };
-    await expect(multiPodCreate(multiPodInput)).rejects.toThrow(
-      'Pod member cannot reference earlier pod',
-    );
-  });
-
-  test('Pod admins cannot reference later pods in the create', async () => {
-    jest.spyOn(ethers, 'Contract').mockReturnValueOnce({
-      getNextAvailablePodId: jest.fn().mockResolvedValueOnce({
-        toNumber: jest.fn().mockReturnValueOnce(100),
-      }),
-    });
-
-    const modifiedInput = multiPodInput;
-    // Referencing a later pod should throw.
-    modifiedInput[0] = { ...modifiedInput[0], admin: 'gov-nauts' };
-    await expect(multiPodCreate(multiPodInput)).rejects.toThrow(
-      'Pod admin cannot reference later pods',
-    );
+    await expect(multiPodCreate(modifiedInput)).rejects.toThrow('No pod had the label of');
   });
 });
