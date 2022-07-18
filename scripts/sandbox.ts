a5ed694adac36441ec56c8699b5ffae26e2074e0
Will Kim <will@sonar-labs.com>
2022-07-18 14:22:50 -0400

    fix: bubble up revert message on failed propose

--- scripts/sandbox.ts
+++ scripts/sandbox.ts
@@ -5,16 +5,16 @@ import { adminPodAddress } from '../env.json';
 import { setup, sleep } from './utils';
 
 async function main() {
-  const { walletOne, dummyAccount } = setup(1);
+  const { walletOne, dummyAccount } = setup(4);
 
-  const pod = await getPod('eject.pod.xyz');
+  const pod = await getPod('multi-child2.pod.eth');
   console.log('pod', pod);
-  await pod.propose(await pod.burnMember(walletOne.address), walletOne.address);
+  await pod.propose(await pod.ejectSafe(), walletOne.address);
 
-  await pod.propose(
-    (await pod.burnMember(walletOne.address)) as { data: string; to: string },
-    walletOne.address,
-  );
+  // await pod.propose(
+  //   (await pod.burnMember(walletOne.address)) as { data: string; to: string },
+  //   walletOne.address,
+  // );
 
   // console.log('dummyAccount', dummyAccount);
   // await pod.ejectSafe(dummyAccount);
