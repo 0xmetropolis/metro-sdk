6972554321cd68996f137d9af3f5420a94fbede8
Will Kim <will@sonar-labs.com>
2022-08-26 10:47:12 -0400

    fix: allow custom subgraph queries

--- src/lib/services/subgraph.ts
+++ src/lib/services/subgraph.ts
@@ -67,3 +67,59 @@ export async function fetchAdminPodIds(address: string): Promise<number[]> {
   const podIds = adminPods.map(pod => parseInt(pod, 10));
   return podIds;
 }
+
+/**
+ * Makes a custom call on the subgraph by passing a query in directly.
+ * Note that this data will likely require manual transformation to be usable.
+ *
+ * Example: Making a call to retrieve pod IDs 1 through 3:
+ *
+ * ```js
+ * await customSubgraphQuery(`{
+ *    pods(where: {
+ *      id_in: [1, 2, 3]
+ *    }) {
+ *      id
+ *      users {
+ *        id
+ *      }
+ *      admin
+ *    }
+ *  }`
+ *
+ * Which will return:
+ *
+ * ```
+ * {
+ *  pods: [
+ *    {
+ *      id: '1',
+ *      users: [Array],
+ *      admin: '0x0000000000000000000000000000000000000000'
+ *    },
+ *    {
+ *      id: '2',
+ *      users: [Array],
+ *      admin: '0x0000000000000000000000000000000000000000'
+ *    },
+ *    {
+ *      id: '3',
+ *      users: [Array],
+ *      admin: '0x4b4c43f66ec007d1dbe28f03dac975aab5fbb888'
+ *    }
+ *  ]
+ * }
+ *  ```
+ *
+ * Note these are not Pod objects as defined by the SDK.
+ *
+ * @param query - GraphQL query
+ * @returns
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export async function customSubgraphQuery(query: string): Promise<any> {
+  const { data } = await axios.post(config.subgraphUrl, {
+    query,
+  });
+  return data.data;
+}
