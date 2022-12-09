3b6332f30b457c13be51fd364cf60e1c0f463dd4
Daniel Thompson <DanThomp507@gmail.com>
2022-12-09 15:30:15 +0000

    fix: clean up tests

--- test/pod.persona.test.ts
+++ test/pod.persona.test.ts
@@ -10,22 +10,26 @@ import { infuraKey } from '../env.json';
 // Some of these tests run long.
 jest.setTimeout(7500);
 
-const expectedPod = {
-  admin: '0xf0C7d25c942264D6F21871c05d3dB3b98344b499',
-  id: 6,
-  name: '1-member-pods.pod.eth',
-  safe: '0xC99cE75a83B068DABF48c48f91C62a30fe0afBd2',
-  member: '0x61De0bbb6C8215Af3f821FE4884A28bc737f98D3', // random member
-};
+const adminAddress = '0xf0C7d25c942264D6F21871c05d3dB3b98344b499';
+const adminPodAddress = '0xa60DDcba1C0160595BA435b7fed98783EC13cE05';
+const podId = 6;
+const podName = '1-member-pods.pod.eth';
+const adminPodName = '1-root-as-admin.pod.eth';
+const podAddress = '0xC99cE75a83B068DABF48c48f91C62a30fe0afBd2';
+const subPodAddress = '0x203D96211d5f0e0De9788302505dd37D26a28005';
+const podMemberOne = '0x61De0bbb6C8215Af3f821FE4884A28bc737f98D3';
+const podMemberTwo = '0x85760ef61c0ccB7BCC4C7A0116d80D59D92e736d';
+const podMemberThree = '0x08A9C4898e045A4E176C2cbf4Ca86aEf9f6EA478';
+const adminPodAddressTwo = '0xBe71ECaA104645ab78ed62A52763b2854e6DaD2E';
 
 function mockGetPodFetchersByAddress(opts?: { overrideAdmin?: string }) {
-  const admin = opts?.overrideAdmin ? opts.overrideAdmin : expectedPod.admin;
+  const admin = opts?.overrideAdmin ? opts.overrideAdmin : adminAddress;
   jest.spyOn(fetchers, 'getPodFetchersByAddressOrEns').mockResolvedValueOnce({
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     Controller: { podAdmin: jest.fn().mockResolvedValueOnce(admin) },
     Safe: {
-      address: expectedPod.safe,
+      address: podAddress,
       nonce: jest.fn().mockResolvedValueOnce({
         toNumber: jest.fn().mockImplementation(() => 5),
       }),
@@ -33,8 +37,8 @@ function mockGetPodFetchersByAddress(opts?: { overrideAdmin?: string }) {
         toNumber: jest.fn().mockImplementation(() => 10),
       }),
     },
-    podId: expectedPod.id,
-    Name: { name: expectedPod.name },
+    podId: podId,
+    Name: { name: podName },
   });
 }
 
@@ -50,181 +54,167 @@ beforeEach(() => {
   jest.restoreAllMocks();
 });
 
-test('Mint via admin persona throws if signer is not included', async () => {
-  mockGetPodFetchersByAddress();
-  const pod = await getPod(expectedPod.name);
-  pod.getPersonas = jest.fn().mockResolvedValue([{ type: 'admin', address: expectedPod.admin }]);
-  const personas = await pod.getPersonas(expectedPod.admin);
-  await expect(pod.callAsPersona(pod.mintMember, [expectedPod.admin], personas[0])).rejects.toThrow(
-    'Expected sender to be signer, but received',
-  );
-});
-
-test('Mint via admin persona calls the function as an admin', async () => {
-  mockGetPodFetchersByAddress();
-  const pod = await getPod(expectedPod.name);
-  pod.getPersonas = jest.fn().mockResolvedValue([{ type: 'admin', address: expectedPod.admin }]);
-  const personas = await pod.getPersonas(expectedPod.admin);
-
-  const mockSigner = {
-    getAddress: jest.fn().mockResolvedValueOnce(expectedPod.admin),
-  };
-
-  const mockMint = jest.fn();
-  jest.spyOn(utils, 'getMetropolisContract').mockReturnValueOnce({
-    mint: mockMint,
+describe('admin member persona', () => {
+  test('Mint via admin persona throws if signer is not included', async () => {
+    mockGetPodFetchersByAddress();
+    const pod = await getPod(podName);
+    pod.getPersonas = jest.fn().mockResolvedValue([{ type: 'admin', address: adminAddress }]);
+    const personas = await pod.getPersonas(adminAddress);
+    await expect(pod.callAsPersona(pod.mintMember, [adminAddress], personas[0])).rejects.toThrow(
+      'Expected sender to be signer, but received',
+    );
   });
 
-  await pod.callAsPersona(pod.mintMember, [expectedPod.admin], personas[0], mockSigner);
-  // expect(mockMint).toHaveBeenCalled();
-  expect(mockMint).toHaveBeenCalledWith(
-    expectedPod.admin,
-    expectedPod.id,
-    ethers.constants.HashZero,
-  );
-});
+  test('Mint via admin persona calls the function as an admin', async () => {
+    mockGetPodFetchersByAddress();
+    const pod = await getPod(podName);
+    pod.getPersonas = jest.fn().mockResolvedValue([{ type: 'admin', address: adminAddress }]);
+    const personas = await pod.getPersonas(adminAddress);
 
-test('Mint via admin persona throws if the signer is not the admin', async () => {
-  mockGetPodFetchersByAddress();
-  const pod = await getPod(expectedPod.name);
-  pod.getPersonas = jest.fn().mockResolvedValue([{ type: 'admin', address: expectedPod.admin }]);
-  const personas = await pod.getPersonas(expectedPod.admin);
+    const mockSigner = {
+      getAddress: jest.fn().mockResolvedValueOnce(adminAddress),
+    };
 
-  const mockSigner = {
-    getAddress: jest.fn().mockResolvedValueOnce(expectedPod.member),
-  };
+    const mockMint = jest.fn();
+    jest.spyOn(utils, 'getMetropolisContract').mockReturnValueOnce({
+      mint: mockMint,
+    });
 
-  const mockMint = jest.fn();
-  jest.spyOn(utils, 'getMetropolisContract').mockReturnValueOnce({
-    mint: mockMint,
+    await pod.callAsPersona(pod.mintMember, [adminAddress], personas[0], mockSigner);
+    // expect(mockMint).toHaveBeenCalled();
+    expect(mockMint).toHaveBeenCalledWith(adminAddress, podId, ethers.constants.HashZero);
   });
 
-  await expect(
-    pod.callAsPersona(pod.mintMember, [expectedPod.admin], personas[0], mockSigner),
-  ).rejects.toThrow('Signer was not admin');
-});
+  test('Mint via admin persona throws if the signer is not the admin', async () => {
+    mockGetPodFetchersByAddress();
+    const pod = await getPod(podName);
+    pod.getPersonas = jest.fn().mockResolvedValue([{ type: 'admin', address: adminAddress }]);
+    const personas = await pod.getPersonas(adminAddress);
 
-test('Mint via member persona creates a proposal', async () => {
-  mockGetPodFetchersByAddress();
-  jest.spyOn(axios, 'post').mockResolvedValueOnce(constructGqlGetUsers([expectedPod.member]));
-  const pod = await getPod(expectedPod.name);
-  pod.getPersonas = jest.fn().mockResolvedValue([{ type: 'member', address: expectedPod.member }]);
-  const personas = await pod.getPersonas(expectedPod.member);
+    const mockSigner = {
+      getAddress: jest.fn().mockResolvedValueOnce(podMemberOne),
+    };
 
-  const create = jest
-    .spyOn(createSafe, 'createSafeTransaction')
-    .mockResolvedValue(getSafeTransactionFixture());
+    const mockMint = jest.fn();
+    jest.spyOn(utils, 'getMetropolisContract').mockReturnValueOnce({
+      mint: mockMint,
+    });
 
-  await pod.callAsPersona(pod.mintMember, [userAddress2], personas[0]);
-
-  expect(create).toHaveBeenCalled();
-});
-
-test('Mint via member persona throws if the persona address is not a member', async () => {
-  mockGetPodFetchersByAddress();
-  jest.spyOn(axios, 'post').mockResolvedValueOnce(constructGqlGetUsers([expectedPod.member]));
-  const pod = await getPod(expectedPod.name);
-  pod.getPersonas = jest.fn().mockResolvedValue([{ type: 'member', address: userAddress2 }]);
-  const personas = await pod.getPersonas(expectedPod.member);
-
-  await expect(pod.callAsPersona(pod.mintMember, [userAddress2], personas[0])).rejects.toThrow(
-    'Sender must be a member of this pod or one of its sub pods',
-  );
-});
-
-test('Mint via adminPodMember persona creates a proposal', async () => {
-  mockGetPodFetchersByAddress({
-    overrideAdmin: '0xa60DDcba1C0160595BA435b7fed98783EC13cE05',
+    await expect(
+      pod.callAsPersona(pod.mintMember, [adminAddress], personas[0], mockSigner),
+    ).rejects.toThrow('Signer was not admin');
   });
-  jest
-    .spyOn(axios, 'post')
-    .mockResolvedValueOnce(constructGqlGetUsers(['0xf0C7d25c942264D6F21871c05d3dB3b98344b499']));
-  const create = jest
-    .spyOn(createSafe, 'createSafeTransaction')
-    .mockResolvedValue(getSafeTransactionFixture());
-
-  const pod = await getPod('1-root-as-admin.pod.eth');
-  const personas = await pod.getPersonas('0x85760ef61c0ccB7BCC4C7A0116d80D59D92e736d');
-
-  await pod.callAsPersona(
-    pod.mintMember,
-    [userAddress2],
-    personas[0],
-    '0x85760ef61c0ccB7BCC4C7A0116d80D59D92e736d',
-  );
-  expect(create).toHaveBeenCalled();
 });
 
-test('Mint via adminPodMember persona should throw if sender is not an admin pod member', async () => {
-  jest
-    .spyOn(axios, 'post')
-    .mockResolvedValueOnce(constructGqlGetUsers(['0xf0C7d25c942264D6F21871c05d3dB3b98344b499']));
+describe('member persona', () => {
+  test('Mint via member persona creates a proposal', async () => {
+    mockGetPodFetchersByAddress();
+    jest.spyOn(axios, 'post').mockResolvedValueOnce(constructGqlGetUsers([podMemberOne]));
+    const pod = await getPod(podName);
+    pod.getPersonas = jest.fn().mockResolvedValue([{ type: 'member', address: podMemberOne }]);
+    const personas = await pod.getPersonas(podMemberOne);
 
-  const pod = await getPod('1-root-as-admin.pod.eth');
-  pod.getPersonas = jest.fn().mockResolvedValue([
-    {
-      type: 'adminPodMember',
-      address: '0xBe71ECaA104645ab78ed62A52763b2854e6DaD2E',
-    },
-  ]);
-  const personas = await pod.getPersonas('0x08A9C4898e045A4E176C2cbf4Ca86aEf9f6EA478');
+    const create = jest
+      .spyOn(createSafe, 'createSafeTransaction')
+      .mockResolvedValue(getSafeTransactionFixture());
 
-  await expect(
-    pod.callAsPersona(pod.mintMember, [userAddress2], personas[0], userAddress2),
-  ).rejects.toThrow('Sender must be a member of the admin pod');
+    await pod.callAsPersona(pod.mintMember, [userAddress2], personas[0]);
+
+    expect(create).toHaveBeenCalled();
+  });
+
+  test('Mint via member persona throws if the persona address is not a member', async () => {
+    mockGetPodFetchersByAddress();
+    jest.spyOn(axios, 'post').mockResolvedValueOnce(constructGqlGetUsers([podMemberOne]));
+    const pod = await getPod(podName);
+    pod.getPersonas = jest.fn().mockResolvedValue([{ type: 'member', address: userAddress2 }]);
+    const personas = await pod.getPersonas(podMemberOne);
+
+    await expect(pod.callAsPersona(pod.mintMember, [userAddress2], personas[0])).rejects.toThrow(
+      'Sender must be a member of this pod or one of its sub pods',
+    );
+  });
 });
 
-test('Mint via subPodMember persona creates a proposal', async () => {
-  mockGetPodFetchersByAddress();
-  jest
-    .spyOn(axios, 'post')
-    .mockResolvedValue(constructGqlGetUsers(['0x85760ef61c0ccB7BCC4C7A0116d80D59D92e736d']));
-  const create = jest
-    .spyOn(createSafe, 'createSafeTransaction')
-    .mockResolvedValue(getSafeTransactionFixture());
+describe('admin member pod persona', () => {
+  test('Mint via adminPodMember persona creates a proposal', async () => {
+    mockGetPodFetchersByAddress({
+      overrideAdmin: adminPodAddress,
+    });
+    jest.spyOn(axios, 'post').mockResolvedValueOnce(constructGqlGetUsers([adminAddress]));
+    const create = jest
+      .spyOn(createSafe, 'createSafeTransaction')
+      .mockResolvedValue(getSafeTransactionFixture());
 
-  const pod = await getPod('1-member-pods.pod.eth');
-  const personas = await pod.getPersonas('0x85760ef61c0ccB7BCC4C7A0116d80D59D92e736d');
+    const pod = await getPod(adminPodName);
+    const personas = await pod.getPersonas(podMemberTwo);
 
-  await pod.callAsPersona(
-    pod.mintMember,
-    [userAddress2],
-    personas[0],
-    '0x85760ef61c0ccB7BCC4C7A0116d80D59D92e736d',
-  );
-  expect(create).toHaveBeenCalled();
+    await pod.callAsPersona(pod.mintMember, [userAddress2], personas[0], podMemberTwo);
+    expect(create).toHaveBeenCalled();
+  });
+
+  test('Mint via adminPodMember persona should throw if sender is not an admin pod member', async () => {
+    jest.spyOn(axios, 'post').mockResolvedValueOnce(constructGqlGetUsers([adminAddress]));
+
+    const pod = await getPod(adminPodName);
+    pod.getPersonas = jest.fn().mockResolvedValue([
+      {
+        type: 'adminPodMember',
+        address: adminPodAddressTwo,
+      },
+    ]);
+    const personas = await pod.getPersonas(podMemberThree);
+
+    await expect(
+      pod.callAsPersona(pod.mintMember, [userAddress2], personas[0], userAddress2),
+    ).rejects.toThrow('Sender must be a member of the admin pod');
+  });
 });
 
-test('Mint via subPodMember persona should throw if sender is not a sub pod member', async () => {
-  jest
-    .spyOn(axios, 'post')
-    .mockResolvedValueOnce(constructGqlGetUsers(['0xf0C7d25c942264D6F21871c05d3dB3b98344b499']));
+describe('subd pod member persona', () => {
+  test('Mint via subPodMember persona creates a proposal', async () => {
+    mockGetPodFetchersByAddress();
+    jest.spyOn(axios, 'post').mockResolvedValue(constructGqlGetUsers([podMemberTwo]));
+    const create = jest
+      .spyOn(createSafe, 'createSafeTransaction')
+      .mockResolvedValue(getSafeTransactionFixture());
 
-  const pod = await getPod('1-root-as-admin.pod.eth');
-  pod.getPersonas = jest.fn().mockResolvedValue([
-    {
-      type: 'subPodMember',
-      address: '0x203D96211d5f0e0De9788302505dd37D26a28005',
-    },
-  ]);
-  const personas = await pod.getPersonas('0x08A9C4898e045A4E176C2cbf4Ca86aEf9f6EA478');
+    const pod = await getPod(podName);
+    const personas = await pod.getPersonas(podMemberTwo);
 
-  await expect(
-    pod.callAsPersona(pod.mintMember, [userAddress2], personas[0], userAddress2),
-  ).rejects.toThrow('Sub pod is not a member of this pod');
-});
+    await pod.callAsPersona(pod.mintMember, [userAddress2], personas[0], podMemberTwo);
+    expect(create).toHaveBeenCalled();
+  });
 
-test('Mint via subPodMember persona should throw if the sub pod is not a member of the pod', async () => {
-  const pod = await getPod(expectedPod.name);
-  pod.getPersonas = jest.fn().mockResolvedValue([
-    {
-      type: 'subPodMember',
-      address: '0x203D96211d5f0e0De9788302505dd37D26a28005',
-    },
-  ]);
-  const personas = await pod.getPersonas('0x08A9C4898e045A4E176C2cbf4Ca86aEf9f6EA478');
+  test('Mint via subPodMember persona should throw if sender is not a sub pod member', async () => {
+    jest.spyOn(axios, 'post').mockResolvedValueOnce(constructGqlGetUsers([adminAddress]));
 
-  await expect(pod.callAsPersona(pod.mintMember, [userAddress2], personas[0])).rejects.toThrow(
-    'Sub pod is not a member of this pod',
-  );
+    const pod = await getPod(adminPodName);
+    pod.getPersonas = jest.fn().mockResolvedValue([
+      {
+        type: 'subPodMember',
+        address: subPodAddress,
+      },
+    ]);
+    const personas = await pod.getPersonas(podMemberThree);
+
+    await expect(
+      pod.callAsPersona(pod.mintMember, [userAddress2], personas[0], userAddress2),
+    ).rejects.toThrow('Sub pod is not a member of this pod');
+  });
+
+  test('Mint via subPodMember persona should throw if the sub pod is not a member of the pod', async () => {
+    const pod = await getPod(podName);
+    pod.getPersonas = jest.fn().mockResolvedValue([
+      {
+        type: 'subPodMember',
+        address: subPodAddress,
+      },
+    ]);
+    const personas = await pod.getPersonas(podMemberThree);
+
+    await expect(pod.callAsPersona(pod.mintMember, [userAddress2], personas[0])).rejects.toThrow(
+      'Sub pod is not a member of this pod',
+    );
+  });
 });
