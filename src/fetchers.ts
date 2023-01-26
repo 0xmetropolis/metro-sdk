b09c31caa23b435a5e2d56881fff5417316e0d4e
John Sterlacci <johntsterlacci@gmail.com>
2023-01-26 10:44:22 -0500

    fix: allows pods to have non pod ens names

--- src/fetchers.ts
+++ src/fetchers.ts
@@ -1,9 +1,55 @@
 import { ethers } from 'ethers';
 import { getDeployment, getControllerByAddress } from '@orcaprotocol/contracts';
 import ENS from '@ensdomains/ensjs';
+import EthersAdapter from '@safe-global/safe-ethers-lib';
+import SafeSdk from '@safe-global/safe-core-sdk';
 import getEns from './lib/services/ens';
 import { config } from './config';
 import { getGnosisSafeContract } from './lib/utils';
+
+/**
+ * Returns Controller address for a given pod ID
+ * @param podId
+ * @returns controllerAddress
+ */
+async function getControllerAddressByPodId(podId) {
+  const { network, multicall } = config;
+
+  // Member token tracks Controller for a given pod ID
+  const memberTokenDeployment = getDeployment('MemberToken', network);
+  const MemberToken = new ethers.Contract(
+    memberTokenDeployment.address,
+    memberTokenDeployment.abi,
+    multicall,
+  );
+
+  return MemberToken.memberController(podId);
+}
+
+/**
+ * Returns Controller address for a given pod address
+ * @param address
+ * @returns controllerAddress
+ */
+async function getControllerAddressByPodAddress(address) {
+  const { multicall } = config;
+
+  const ethAdapter = new EthersAdapter({
+    ethers,
+    signerOrProvider: multicall,
+  });
+  let safeSdk;
+  try {
+    // will fail if address is not a safe
+    safeSdk = await SafeSdk.create({
+      ethAdapter,
+      safeAddress: address,
+    });
+  } catch (err) {
+    throw new Error('Address is not a safe');
+  }
+  return safeSdk.getGuard();
+}
 
 /**
  * Returns Controller, ENS Name and pod ID for a given pod address.
@@ -37,24 +83,35 @@ export async function getPodFetchersByAddressOrEns(identifier: string): Promise<
     address = await multicall.resolveName(Name.name);
   }
 
-  const podId = await Name.getText('podId');
-  if (!podId) throw new Error('No podId on ENS found');
+  let podId = await Name.getText('podId');
+  let controllerAddress;
 
-  // Member token tracks Controller for a given pod ID
-  const memberTokenDeployment = getDeployment('MemberToken', network);
-  const MemberToken = new ethers.Contract(
-    memberTokenDeployment.address,
-    memberTokenDeployment.abi,
-    multicall,
-  );
+  if (podId) {
+    controllerAddress = await getControllerAddressByPodId(podId);
+  } else {
+    // fallback checker for pods without .pod ENS names
+    controllerAddress = await getControllerAddressByPodAddress(address);
+  }
 
-  const controllerAddress = await MemberToken.memberController(podId);
-  const controllerDeployment = getControllerByAddress(controllerAddress, network);
+  let controllerDeployment;
+
+  try {
+    // we can not guarantee that the controller adddress is a valid controller
+    controllerDeployment = getControllerByAddress(controllerAddress, network);
+  } catch (err) {
+    throw new Error('Address is not a pod');
+  }
+
   const Controller = new ethers.Contract(
     controllerDeployment.address,
     controllerDeployment.abi,
     multicall,
   );
+
+  if (!podId) {
+    // if we don't have a podId, we need to fetch it from the controller
+    podId = await Controller.safeToPodId(address);
+  }
 
   const Safe = getGnosisSafeContract(address, multicall);
 
